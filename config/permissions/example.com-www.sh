3ae45f9c7a387d0eb0d604a8f1ea3bdfdebbc174
Will Power <github@willpoweredinc.co.uk>
2017-11-25 15:21:09 +0000

    lets not make permissions confusing, lets use users properly

--- config/permissions/example.com-www.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ $1 ] && /organisation/git/config/permissions/_standard.sh
-
-chmod -R 775 /organisation/websites/example.com/www/public_html/uploads/{downloads,photos}
-
-[ $1 ] && echo "www.example.com permissions have been reset"
